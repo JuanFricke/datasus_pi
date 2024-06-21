1ef6366fc19d204699e9374bae4f1397fa0d7871
Juan Paulo Fricke <89048734+JuanFricke@users.noreply.github.com>
2024-06-21 16:00:34 -0300

    Update app.py for render

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 import os
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
-
-
-app = Flask(__name__)
 
 def get_body_class(route):
     # Dicionário de rotas para classes
